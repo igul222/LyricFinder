44d5430816b7761fe1ead4d79b2f37fa06badec0
Ishaan <igul222@gmail.com>
2011-02-05 20:57:37 -0500

    set up basic unit testing framework

---
+++ Tests/Tests.h
@@ -0,0 +1,16 @@
+//
+//  Tests.h
+//  Tests
+//
+//  Created by Ishaan Gulrajani on 2/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tests : SenTestCase {
+@private
+
+}
+
+@end
---
+++ Tests/Tests.m
@@ -0,0 +1,24 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by Ishaan Gulrajani on 2/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tests.h"
+
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+@end
